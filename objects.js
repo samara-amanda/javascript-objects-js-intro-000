416bd2346a22dbf7bcce24262d1af3962838d4f1
Samara Grant <samara.amanda01@gmail.com>
2020-06-22 00:20:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,4 +7,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
   'Slowdive': 'Alison',
   'My Bloody Valentine': 'Sometimes',
   'Phil Ochs': "Here's to the State of Mississippi"
+  playlist[artistName] = 'Slowdive'
 }
