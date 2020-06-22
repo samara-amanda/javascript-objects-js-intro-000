17668ec59db0de7766c02ad88e4750126d841f2f
Samara Grant <samara.amanda01@gmail.com>
2020-06-22 00:50:05 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,3 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = 'Slowdive'
   playlist[songTitle] = 'Alison'
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+}
