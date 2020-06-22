760e2b7e43e7334a8cf874a51332c8b43c0196c3
Samara Grant <samara.amanda01@gmail.com>
2020-06-22 00:08:26 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = "Phil Ochs"
-  playlist.songTitle = "Here's to the State of Mississippi"
+  artistName = "Phil Ochs"
+  songTitle = "Here's to the State of Mississippi"
 }
